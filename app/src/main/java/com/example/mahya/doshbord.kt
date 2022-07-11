b35438273e14f82acacf7a6cedd745cae71212b3
LENOVO <operatorkareetoo1@gmail.com>
2022-07-11 08:35:49 +0430

    first commit

---
+++ app/src/main/java/com/example/mahya/doshbord.kt
@@ -0,0 +1,11 @@
+package com.example.mahya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class doshbord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doshbord)
+    }
+}
