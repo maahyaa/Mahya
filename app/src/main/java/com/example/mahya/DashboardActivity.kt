22090a4e16b1d6dd213c2ed709ab2da91a934dd9
LENOVO <operatorkareetoo1@gmail.com>
2022-07-12 08:59:23 +0430

    first commit

--- app/src/main/java/com/example/mahya/DashboardActivity.kt
+++ app/src/main/java/com/example/mahya/DashboardActivity.kt
@@ -2,10 +2,15 @@ package com.example.mahya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class doshbord : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doshbord)
+        setContentView(R.layout.activity_dashboard)
+        Toast.makeText(this, "enjoy travelling", Toast.LENGTH_SHORT).show()
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/mahya/IntentExampleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mahya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntentExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_example)
+    }
+}
