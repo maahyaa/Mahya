3a6cd00965beb587a7499ba15f8f6a4814750bf4
LENOVO <operatorkareetoo1@gmail.com>
2022-07-12 11:41:51 +0430

    first commit

--- app/src/main/java/com/example/mahya/DashboardActivity.kt
+++ app/src/main/java/com/example/mahya/DashboardActivity.kt
@@ -3,14 +3,22 @@ package com.example.mahya
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
+import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        Toast.makeText(this, "enjoy travelling", Toast.LENGTH_SHORT).show()
+
+       val backbutton:AppCompatImageButton= findViewById(R.id.back)
+        val logOutButton: AppCompatImageButton = findViewById(R.id.city)
+        val location :AppCompatImageButton=findViewById(R.id.location)
+        val card:MaterialCardView=findViewById(R.id.card_view_vespa)
+        backbutton.setOnClickListener {
+       Toast.makeText(this,"enjoy traveling",Toast.LENGTH_SHORT).show()
+
+        }
     }
-
-
 
 }
